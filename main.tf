22d3d48d5eb369e06e92206b73ff25ff8a95cdab
Kelsi Hoyle <kelsi.hoyle@hashicorp.com>
2025-02-20 13:00:52 -0700

    update to working version

--- main.tf
+++ main.tf
@@ -1,10 +1,29 @@
+terraform {
+  cloud {
+    # local dev testing
+    workspaces {
+      name = "terraform-random"
+    }
+
+    organization = "hashicorp"
+    hostname = "tfcdev-da6ca1b6.ngrok.app"
+  }
+
+  # amd provider
+  # required_providers {
+  #    discord = {
+  #    source = "prod9/discord"
+  #    version = "0.0.4-p9"
+  #  }
+  # }
+}
+
 resource "random_id" "random" {
   keepers = {
     uuid = uuid()
   }
 
   byte_length = 12
- 
 }
 
 output "random" {
